8873710659e18bd8f833a9e60ce57b702ada7854
KhoaAir win <KhoaAir@gmail.com>
2023-04-04 21:37:06 +0700

    finish error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import Landing from './pages/Landing';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Landing, Error, Register, Dashboard } from './pages';
 
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='landing' element={<Landing />} />
+        <Route path='register' element={<Register />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import img from '../assets/images/logo.svg';
+
+const logo = () => {
+  return <img src={img} alt='Jobster logo' className='logo' />;
+};
+export default logo;
