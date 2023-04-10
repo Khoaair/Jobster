2f1afe752afd13ec3b7fd45877aa91961e693f9a
KhoaAIr <KhoaAir@gmail.com>
2023-04-10 16:34:51 +0700

    profile setup

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,9 +1,9 @@
 import { NavLink } from 'react-router-dom';
-import links from '../utils/Links';
+import links from '../utils/links';
 
 import React from 'react';
 
-const NavLinks = toggleSidebar => {
+const NavLinks = ({ toggleSidebar }) => {
   return (
     <div className='nav-links'>
       {links.map(link => {
@@ -11,12 +11,12 @@ const NavLinks = toggleSidebar => {
         return (
           <NavLink
             to={path}
+            end
             key={id}
             onClick={toggleSidebar}
             className={({ isActive }) => {
               return isActive ? 'nav-link active' : 'nav-link';
             }}
-            end
           >
             <span className='icon'>{icon}</span>
             {text}
--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { useState } from 'react';
+import { FormRow } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const { isLoading, user } = useSelector(store => store.user);
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    name: user?.name || '',
+    email: user?.email || '',
+    lastName: user?.lastName || '',
+    location: user?.location || '',
+  });
+
+  const { name, email, lastName, location } = userData;
+  const handleSubmit = e => {
+    e.preventDefaut();
+    if (!name || !email || !lastName || !location) {
+      toast.error('please fill out all fields');
+      return;
+    }
+  };
+
+  const handleChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+  return (
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='name'
+            value={name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='text'
+            name='lastName'
+            labelText='Last Name'
+            value={lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='text'
+            name='email'
+            value={email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='text'
+            name='location'
+            value={location}
+            handleChange={handleChange}
+          />
+          <button type='submit' className='btn btn-block' disabled={isLoading}>
+            {isLoading ? 'Please Wait... ' : 'save changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Profile;
